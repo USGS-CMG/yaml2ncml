a338d43fabdca343a721d8b6caacadd5a46cea4b
ocefpaf <ocefpaf@gmail.com>
2015-06-26 14:37:41 -0300

    Non-notebook version

--- yaml2ncml/yaml2ncml.py
+++ yaml2ncml/yaml2ncml.py
@@ -1,98 +1,68 @@
 import os
-import yaml
-import pyncml
 import netCDF4
-from StringIO import StringIO
+
+import yaml
 
 
-__all__ = ['yaml2ncml']
-
-
-# Map ROMS variables to CF standard_names.
-cf = {'zeta':'sea_surface_height_above_datum',
-      'temp':'sea_water_potential_temperature',
-      'salt':'sea_water_salinity',
-         'u':'x_sea_water_velocity',
-         'v':'y_sea_water_velocity',
-      'ubar':'barotropic_x_sea_water_velocity',
-      'vbar':'barotropic_y_sea_water_velocity',
-     'Hwave':'sea_surface_wave_significant_height'}
-
-# Couldn't get this to work.
-# stream = open(StringIO(x))
-
-# So read file instead.
-def load_tempplate(yamlfile):
-    with open(yamlfile, 'r') as stream:
-        a = yaml.load(stream)
+def str_att(name, value):
+    if isinstance(value, list):
+        value = ','.join(value)
+    msg = '  <attribute name="{:s}" type="String" value="{:s}"/>\n'
+    return msg.format(name, value)
 
 
 def header():
-    str='<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns="http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n'
-    str += str_att('Conventions','CF-1.6, SGRID-0.1, ACDD-1.3')
-    str += str_att('cdm_data_type','Grid')
-    return str
+    text = '<?xml version="1.0" encoding="UTF-8"?>\n<netcdf xmlns='
+    text += '"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2">\n'
+    text += str_att('Conventions','CF-1.6, SGRID-0.1, ACDD-1.3')
+    text += str_att('cdm_data_type','Grid')
+    return text
 
 
-def footer(str):
-    str += '</netcdf>\n'
-    return str
+def footer(text):
+    text += '</netcdf>\n'
+    return text
 
-
-def str_att(name,value):
-    if isinstance(value, list):
-        value = ','.join(value)
-    return '  <attribute name="{:s}" type="String" value="{:s}"/>\n'.format(name,value)
-
-
-def add_global_atts(str,a):
+def add_global_atts(text, a):
     d = a['dataset']
-    for key, value in d.iteritems():
-        # handle simple attribute pairs first
-        if key in ['id','license','summary','title','project','naming_authority','references','acknowledgements']:
-            str += str_att(key,value)
-            
-        elif key in ['creator','publisher']:
-
+    for key, value in d.items():
+        # Handle simple attribute pairs first.
+        if key in ['id', 'license', 'summary', 'title', 'project',
+                   'naming_authority', 'references', 'acknowledgments']:
+            text += str_att(key,value)
+        elif key in ['creator', 'publisher']:
             email = value.get("email", None)
             if email:
-                str += str_att('_'.join([key,'email']),email)
-
+                text += str_att('_'.join([key, 'email']), email)
             url = value.get("url", None)
             if url:
-                str += str_att('_'.join([key,'url']),url)
-
+                text += str_att('_'.join([key, 'url']), url)
             name = value.get("name", None)
             if name:
-                str += str_att('_'.join([key,'name']),name)
-                
+                text += str_att('_'.join([key, 'name']), name)
         elif key in ['contributor']:
             role = value.get("role", None)
             if email:
-                str += str_att('_'.join([key,'role']),role)
-            
+                text += str_att('_'.join([key, 'role']), role)
             email = value.get("email", None)
             if email:
-                str += str_att('_'.join([key,'email']),email)
-
+                text += str_att('_'.join([key, 'email']), email)
             url = value.get("url", None)
             if url:
-                str += str_att('_'.join([key,'url']),url)
-
+                text += str_att('_'.join([key, 'url']), url)
             name = value.get("name", None)
             if name:
-                str += str_att('_'.join([key,'name']),name)
-    return str
+                text += str_att('_'.join([key, 'name']), name)
+    return text
 
-
-def add_var_atts(str,a):
-    ncfile=os.path.join(a['aggregation']['dir'],a['aggregation']['sample_file'])
+def add_var_atts(text, a):
+    ncfile = os.path.join(a['aggregation']['dir'],
+                          a['aggregation']['sample_file'])
     nc = netCDF4.Dataset(ncfile)
     ncv = nc.variables
-    
-    # get a list of all variables more than 1D
+
+    # Get a list of all variables more than 1D.
     vars = [var for var, vart in ncv.items() if vart.ndim > 1]
-    
     vars_all = set(vars)
     vars_include = set(a['variables']['include'])
     vars_exclude = set(a['variables']['exclude'])
@@ -101,32 +71,31 @@ def add_var_atts(str,a):
     else:
         if a['variables']['include']:
             vars = list(vars_all.intersection(vars_include))
-        
-    #rho_vars = [var for var, vart in ncv.items() if hasattr(vart,'coordinates') and 'lon_rho' in vart.coordinates and 'lat_rho' in vart.coordinates]    
-    rho_vars = [var for var, vart in ncv.items() if 'eta_rho' in vart.dimensions and 'xi_rho' in vart.dimensions]
-    u_vars = [var for var, vart in ncv.items() if 'eta_u' in vart.dimensions and 'xi_u' in vart.dimensions]
-    v_vars = [var for var, vart in ncv.items() if 'eta_v' in vart.dimensions and 'xi_v' in vart.dimensions]
-    
+    rho_vars = [var for var, vart in ncv.items() if 'eta_rho' in
+                vart.dimensions and 'xi_rho' in vart.dimensions]
+    u_vars = [var for var, vart in ncv.items() if 'eta_u' in
+              vart.dimensions and 'xi_u' in vart.dimensions]
+    v_vars = [var for var, vart in ncv.items() if 'eta_v' in
+              vart.dimensions and 'xi_v' in vart.dimensions]
+
     for var in vars:
-        str += '<variable name="{:s}">\n'.format(var)
+        text += '<variable name="{:s}">\n'.format(var)
         try:
-            str += str_att('standard_name',cf[var])
+            text += str_att('standard_name',cf[var])
         except:
             pass
-        str += str_att('grid','grid')
-        str += str_att('content_coverage_type','modelResult')
+        text += str_att('grid','grid')
+        text += str_att('content_coverage_type','modelResult')
         if var in rho_vars:
-            str += str_att('location','face')
+            text += str_att('location','face')
         elif var in u_vars:
-            str += str_att('location','edge1')
+            text += str_att('location','edge1')
         elif var in v_vars:
-            str += str_att('location','edge2')
-        str += '</variable>\n\n'
-        
-    return str
+            text += str_att('location','edge2')
+        text += '</variable>\n\n'
+    return text
 
-
-def write_grid_var(str):
+def write_grid_var(text):
     grid_var="""<variable name="grid" type="int">
         <attribute name="cf_role" value="grid_topology"/>
         <attribute name="topology_dimension" type="int" value="2"/>
@@ -141,27 +110,35 @@ def write_grid_var(str):
         <attribute name="edge2_coordinates" value="lon_v lat_v"/>
         <attribute name="vertical_dimensions" value="s_rho: s_w (padding: none)"/>
     </variable>\n """
-    str += grid_var
-    return str
+    text += grid_var
+    return text
 
-
-def add_aggregation_scan(str,a):
+def add_aggregation_scan(text, a):
     agg = a['aggregation']
-#    <aggregation dimName="ocean_time" type="joinExisting">
-#        <scan location="." regExp=".*his_case7_ar0fd_[0-9]{4}\.nc$" subdirs="false"/>
-#    </aggregation>
-    str += '<aggregation dimName="{:s}" type="joinExisting">\n'.format(agg['time_var'])
-    str += '<scan location="." regExp="{:s}" subdirs="false"/>\n</aggregation>\n'.format(agg['pattern'])
-    return str
+    text += '<aggregation dimName="{:s}" type="joinExisting">\n'.format(agg['time_var'])
+    text += '<scan location="." regExp="{:s}" subdirs="false"/>\n</aggregation>\n'.format(agg['pattern'])
+    return text
 
+# Map ROMS variables to CF standard_names.
+cf = dict(zeta='sea_surface_height_above_datum',
+          temp='sea_water_potential_temperature',
+          salt='sea_water_salinity',
+          u='x_sea_water_velocity',
+          v='y_sea_water_velocity',
+          ubar='barotropic_x_sea_water_velocity',
+          vbar='barotropic_y_sea_water_velocity',
+          Hwave='sea_surface_wave_significant_height')
 
-def yaml2ncml():
-    str = header()
-    str = add_global_atts(str, a)
-    str = add_var_atts(str, a)
-    str = write_grid_var(str)
-    str = add_aggregation_scan(str, a)
-    str = footer(str)
+if __name__ == '__main__':
+    with open("./tests/roms.yaml", 'r') as stream:
+        a = yaml.load(stream)
 
-    with open('{:s}/test4.ncml'.format(a['aggregation']['dir']),'w') as text_file:
-        text_file.write("{:s}".format(str))
+    text = header()
+    text = add_global_atts(text, a)
+    text = add_var_atts(text, a)
+    text = write_grid_var(text)
+    text = add_aggregation_scan(text, a)
+    text = footer(text)
+
+    with open('{:s}/test6.ncml'.format(a['aggregation']['dir']),'w') as text_file:
+        text_file.write("{:s}".format(text))
