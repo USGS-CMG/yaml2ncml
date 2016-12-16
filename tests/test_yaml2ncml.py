ba4a20ee94355464ec8b35750660f7b8fe0cc3db
ocefpaf <ocefpaf@gmail.com>
2016-12-16 17:13:11 -0300

    Test bad call/better error msg

--- tests/test_yaml2ncml.py
+++ tests/test_yaml2ncml.py
@@ -3,6 +3,11 @@ from __future__ import (absolute_import, division, print_function)
 import subprocess
 import tempfile
 
+import pytest
+import ruamel.yaml as yaml
+
+from yaml2ncml import build
+
 
 def test_call():
     output = subprocess.check_output(['yaml2ncml', 'roms_0.yaml'])
@@ -21,3 +26,16 @@ def test_save_file():
     with open(outfile) as f:
         output = f.read()
     assert output == expected
+
+
+@pytest.fixture
+def load_ymal(fname='roms_1.yaml'):
+    with open(fname, 'r') as stream:
+        yml = yaml.load(stream, Loader=yaml.RoundTripLoader)
+    return yml
+
+
+def test_bad_yaml():
+    with pytest.raises(ValueError):
+        yml = load_ymal(fname='roms_1.yaml')
+        build(yml)
