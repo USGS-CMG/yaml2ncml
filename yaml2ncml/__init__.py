8086720b301954d34a2245df3e4a49d5a56b2821
Rich Signell <rsignell@usgs.gov>
2018-01-31 17:03:54 -0500

    Update __init__.py
    fixing version

--- yaml2ncml/__init__.py
+++ yaml2ncml/__init__.py
@@ -3,7 +3,7 @@ from __future__ import (absolute_import, division, print_function)
 from yaml2ncml.yaml2ncml import main, build
 
 
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 __all__ = [
     'main',
