c36eac0d7bddb45d3c7c1261a3696ba60c384c84
Zafer <zdefne-usgs@users.noreply.github.com>
2019-06-07 17:05:15 -0400

    Update __init__.py

--- yaml2ncml/__init__.py
+++ yaml2ncml/__init__.py
@@ -3,7 +3,7 @@ from __future__ import (absolute_import, division, print_function)
 from yaml2ncml.yaml2ncml import main, build
 
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 __all__ = [
     'main',
